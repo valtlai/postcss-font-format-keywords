bf50a01c65eb57d175a08748b0937a56be2d6dca
Valtteri Laitinen <dev@valtlai.fi>
2020-05-28 00:26:26 +0300

    Add test for single quotes

--- .tape.js
+++ .tape.js
@@ -4,6 +4,10 @@ module.exports = {
 	basic: {
 		message: 'supports basic usage'
 	},
+	single: {
+		message: 'supports single quotes',
+		options: { singleQuote: true }
+	},
 	ignored: {
 		message: 'ignores other usage',
 		expect: 'ignored.css'
