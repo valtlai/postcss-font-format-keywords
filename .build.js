8fe9101ec0acb0dc621aa7aa7b7424a821be15e7
Valtteri Laitinen <dev@valtlai.fi>
2021-05-31 17:35:30 +0300

    Build Node.js version just before publishing

--- .build.js
+++ .build.js
@@ -1,17 +1,6 @@
-// $ deno run --allow-read=. --allow-write=. .build.js
+import build from "https://raw.githubusercontent.com/valtlai/nodefy/1.0.0/mod.js";
 
-const input = Deno.readTextFileSync("index.js");
-
-const denoOutput = input.replace(
-  'from "postcss-value-parser"',
-  'from "https://deno.land/x/postcss_value_parser@v4.1.0/mod.js"',
-);
-
-const cjsOutput = '"use strict";\n\n' + input
-  .replace(/import (\w+) from ("[^"]+")/, "const $1 = require($2)")
-  .replace("export default", "module.exports =");
-
-await Promise.all([
-  Deno.writeTextFile("mod.js", denoOutput),
-  Deno.writeTextFile("index.cjs", cjsOutput),
+build([
+  "https://deno.land/x/postcss_value_parser@v4.1.0/mod.js",
+  "postcss-value-parser",
 ]);
